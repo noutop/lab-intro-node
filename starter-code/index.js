ad18baa3b6ffc6b71213fc421c9732c0fce59546
Noubar Topalian <topalian.noubar@gmail.com>
2020-03-28 17:04:39 +0100

    BERLIN-WDPT-Noubar

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.items=[]
+    this.length=this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    
+    this.items.push(item)
+    this.items.sort((a,b)=>{
+      return a-b;
+    })
+    this.length=this.items.length
+    
+  }
 
-  get(pos) {}
+  get(pos) {
+      if(pos>=this.length){
+        throw new Error("OutOfBounds");
+      }else{
+        return this.items[pos]
+      }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if(this.length===0){
+      throw new Error("EmptySortedList");
+    }else{
+      return Math.max(...this.items)
+    }
+  }
 
-  sum() {}
+  min() {
+    if(this.length===0){
+      throw new Error("EmptySortedList");
+    }else{
+      return Math.min(...this.items)
+    }
+  }
 
-  avg() {}
+  sum() {
+    if(this.length===0){
+      return 0
+    }else{
+      return this.items.reduce((total,num)=>{
+        return total+num;
+      })
+    }
+  }
+
+  avg() {
+    if(this.length===0){
+      throw new Error("EmptySortedList");
+    }else{
+    return this.sum()/this.length
+    }
+  }
 }
 
 module.exports = SortedList;
